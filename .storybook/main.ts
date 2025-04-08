f87294a133cd416f1b3f1d2ff5dc5c490280b951
Vinit Khandal <vinit.khandal@juspay.in>
2025-04-08 11:53:44 +0530

    feat: integrate storybook for button

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,22 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../lib/**/*.mdx', '../lib/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    "../lib/**/*.mdx",
+    "../lib/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
   ],
   framework: {
-    name: '@storybook/react-vite',
-    options: {},
+    name: "@storybook/react-vite",
+    options: {}
   },
   docs: {
-    autodocs: true,
-  },
+    autodocs: true
+  }
 };
 
 export default config;
