9d10425c3052431673f10d2d46f3a5bfad610407
dkrai04 <deepanshu.kumar@juspay.in>
2025-04-08 15:48:10 +0530

    seperated out the splittag component

--- lib/components/Tag/SplitTag.tsx
+++ lib/components/Tag/SplitTag.tsx
@@ -9,25 +9,26 @@ import { renderSlot } from './utils';
  * SplitTag Component - Tag with two distinct sections (left and right)
  */
 const SplitTag = React.forwardRef<HTMLDivElement, SplitTagProps>(
-  (
-    {
-      tagStyle = 'rounded',
-      size = 'md',
-      color = 'neutral',
-      leftLabel,
-      rightLabel,
-      leftSlot,
-      rightSlot,
-      className,
-      ...props
-    },
-    ref
-  ) => {
+  ({
+    tagStyle = 'rounded',
+    size = 'md',
+    color = 'neutral',
+    leftLabel,
+    rightLabel,
+    leftSlot,
+    rightSlot,
+    className,
+    ...props
+  }, ref) => {
     const fontSizeClass = themeConfig.euler.tag.sizes[size].fontSize;
     const containerClasses = themeConfig.euler.tag.layout.container;
-
+    
     return (
-      <div ref={ref} className={cn(containerClasses, className)} {...props}>
+      <div
+        ref={ref}
+        className={cn(containerClasses, className)}
+        {...props}
+      >
         <div className={getSplitTagClassNames(tagStyle, size, color, true)}>
           {leftSlot && <span {...renderSlot(leftSlot, size)} />}
           {leftLabel && <span className={fontSizeClass}>{leftLabel}</span>}
@@ -43,4 +44,4 @@ const SplitTag = React.forwardRef<HTMLDivElement, SplitTagProps>(
 
 SplitTag.displayName = 'SplitTag';
 
-export default SplitTag;
+export default SplitTag; 
--- lib/components/Tag/Tag.tsx
+++ lib/components/Tag/Tag.tsx
@@ -1,26 +1,9 @@
 import * as React from 'react';
-import { TagProps, SplitTagProps, TagSize } from './types';
+import { TagProps } from './types';
 import { cn } from '../../utils';
-import { getTagClassNames, getSplitTagClassNames } from './utils';
+import { getTagClassNames } from './utils';
 import { themeConfig } from '../../themeConfig';
-
-/**
- * Renders a slot (icon or custom element) with appropriate styling
- */
-const renderSlot = (slot: React.ReactNode | undefined, size: TagSize) => {
-  if (!slot) return null;
-  
-  const tagTheme = themeConfig.euler.tag;
-  const slotSize = tagTheme.sizes[size].iconSize;
-  const slotGap = tagTheme.sizes[size].gap;
-  const slotClasses = tagTheme.layout.slot;
-  
-  return (
-    <span className={cn(slotClasses, slotSize, slotGap)}>
-      {slot}
-    </span>
-  );
-};
+import { renderSlot } from './utils';
 
 /**
  * Tag Component - Displays labels, categories, statuses, or other metadata
@@ -46,53 +29,14 @@ const Tag = React.forwardRef<HTMLDivElement, TagProps>(
         className={cn(containerClassName, className)}
         {...props}
       >
-        {renderSlot(leadingSlot, size)}
+        {leadingSlot && <span {...renderSlot(leadingSlot, size)} />}
         {label && <span className={fontSizeClass}>{label}</span>}
-        {renderSlot(trailingSlot, size)}
+        {trailingSlot && <span {...renderSlot(trailingSlot, size)} />}
       </div>
     );
   }
 );
 
 Tag.displayName = 'Tag';
-
-/**
- * SplitTag Component - Tag with two distinct sections (left and right)
- */
-export const SplitTag = React.forwardRef<HTMLDivElement, SplitTagProps>(
-  ({
-    tagStyle = 'rounded',
-    size = 'md',
-    color = 'neutral',
-    leftLabel,
-    rightLabel,
-    leftSlot,
-    rightSlot,
-    className,
-    ...props
-  }, ref) => {
-    const fontSizeClass = themeConfig.euler.tag.sizes[size].fontSize;
-    const containerClasses = themeConfig.euler.tag.layout.container;
-    
-    return (
-      <div
-        ref={ref}
-        className={cn(containerClasses, className)}
-        {...props}
-      >
-        <div className={getSplitTagClassNames(tagStyle, size, color, true)}>
-          {renderSlot(leftSlot, size)}
-          {leftLabel && <span className={fontSizeClass}>{leftLabel}</span>}
-        </div>
-        <div className={getSplitTagClassNames(tagStyle, size, color, false)}>
-          {rightLabel && <span className={fontSizeClass}>{rightLabel}</span>}
-          {renderSlot(rightSlot, size)}
-        </div>
-      </div>
-    );
-  }
-);
-
-SplitTag.displayName = 'SplitTag';
 
 export default Tag;
