dd1dd2bcf6ca598c6280691ebc7dc39a3f257753
Vinit Khandal <vinit.khandal@juspay.in>
2025-04-09 18:41:21 +0530

    chore : update tags expanded state

--- lib/components/Tabs/TabsList.tsx
+++ lib/components/Tabs/TabsList.tsx
@@ -2,22 +2,25 @@ import React from 'react';
 import * as TabsPrimitive from '@radix-ui/react-tabs';
 import { cn } from '../../utils';
 import { TabsListProps } from './types';
-import { themeConfig } from '../../themeConfig';
+import { getTabsStyles, getSizeStyles, getBaseStyles, getExpandedStyles } from './utils';
 
 const TabsList = React.forwardRef<
   HTMLDivElement,
   TabsListProps
->(({ className, variant = "underline", size = "md", ...props }, ref) => {
-  const styles = themeConfig.euler.tabs.variant[variant];
-  const sizeStyles = themeConfig.euler.tabs.sizes[size];
+>(({ className, variant = "underline", size = "md", expanded = false, ...props }, ref) => {
+  const styles = getTabsStyles(variant);
+  const sizeStyles = getSizeStyles(size);
+  const baseStyles = getBaseStyles();
+  const expandedStyles = getExpandedStyles(expanded);
   
   return (
     <TabsPrimitive.List
       ref={ref}
       className={cn(
-        "flex w-full items-center",
+        baseStyles,
         styles.list,
-        sizeStyles.height,
+        sizeStyles,
+        expanded && expandedStyles,
         className
       )}
       {...props}
