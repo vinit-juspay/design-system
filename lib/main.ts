9e1f4a6ed19292c5447c7448e16ce2a308ca75cb
dkrai04 <deepanshu.kumar@juspay.in>
2025-04-15 12:10:28 +0530

    main.ts changes

--- lib/main.ts
+++ lib/main.ts
@@ -1,16 +1,9 @@
 import '../src/index.css';
 
 export { default as Button } from './components/Button/Button';
-<<<<<<< HEAD
 export { default as Menu } from './components/Menu/Menu';
 export { default as Checkbox } from './components/common/Checkbox';
 export { default as Tag } from './components/Tag/Tag';
 export { default as SplitTag } from './components/Tag/SplitTag';
 export { default as Tooltip } from './components/Tooltip/Tooltip';
 export { Tabs, TabsList, TabsTrigger, TabsContent } from './components/Tabs';
-=======
-export { default as Tooltip } from './components/Tooltip/Tooltip';
-export { default as Tag } from './components/Tag/Tag';
-export { default as SplitTag } from './components/Tag/SplitTag';
-export { Tabs, TabsList, TabsTrigger, TabsContent } from './components/Tabs';
->>>>>>> 1fb266520415243e8d87074a3773a0620c278796
