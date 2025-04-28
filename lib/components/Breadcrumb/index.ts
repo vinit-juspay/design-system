4153547962c373266d01f40f69a1496f4345e87c
Vinit Khandal <vinit.khandal@juspay.in>
2025-04-28 15:33:56 +0530

    chore: update exports

--- lib/components/Breadcrumb/index.ts
+++ lib/components/Breadcrumb/index.ts
@@ -1,4 +1,3 @@
 export { default as Breadcrumb } from './Breadcrumb';
 export { BreadcrumbItem } from './BreadcrumbItem';
-export { BreadcrumbVariant } from './types';
-export type { BreadcrumbProps, BreadcrumbItemProps } from './types'; 
+export * from './types';
